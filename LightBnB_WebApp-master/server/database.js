8531bbfc1427339ac942b36c31a76be54beb467d
ishpiamazing <ishpiamazing@gmail.com>
2020-09-17 17:31:45 +0000

    get all reservations for a single user code added

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -82,13 +82,27 @@ exports.addUser = addUser;
 
 /// Reservations
 
+
+//4. get all reservations for a user
 /**
  * Get all reservations for a single user.
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  //return getAllProperties(null, 2);
+
+  return pool.query(`SELECT reservations.*,properties.*,AVG(property_reviews.rating) as average_rating
+  FROM properties JOIN reservations
+  ON reservations.property_id = properties.id
+  JOIN property_reviews ON 
+  property_reviews.property_id = properties.id
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date 
+  LIMIT $2;`,[guest_id,limit])
+  .then(res => res.rows)
 }
 exports.getAllReservations = getAllReservations;
 
